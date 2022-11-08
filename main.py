d209ee27f23e8bbf39930af0b3cbf6cbbb9dd163
Tannavi Gaurav <37728386+heyTanvi@users.noreply.github.com>
2022-11-08 22:19:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,207 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from rds_process import rds_handler
 import json
+import requests
+import datetime
+import rds_test
+import boto3
+import psycopg2
+s3 = boto3.client("s3")
 
-#open json file and give it to data variable as a dictionary
-with open("db.json") as data_file:
-    data = json.load(data_file)
-
-#Defining a HTTP request Handler class
-class ServiceHandler(BaseHTTPRequestHandler):
-    #sets basic headers for the server
-
-# server headers
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/json')
-        #reads the length of the Headers
-        length = int(self.headers['Content-Length'])
-        #reads the contents of the request
-        content = self.rfile.read(length)
-        temp = str(content).strip('b\'')
-        self.end_headers()
-        return temp
-
-    #GET Method Defination
-    def do_GET(self):
-        #defining all the headers
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type','text/json')
-            self.end_headers()
-            datas ={}
-            datas["health"]="200"
-            #prints all the keys and values of the json file
-            self.wfile.write(json.dumps(datas).encode())
-        if self.path == '/api/id':
-            key = self._set_headers()
-            rds_ob = rds_handler()
-            result = rds_ob.read(key)
-            print(result)
-            self.wfile.write(json.dumps(result).encode())
-
-    #POST method defination
-    def do_POST(self):
-        if self.path == '/api/create':
-            temp = self._set_headers()
-            rds_ob = rds_handler()
-
-            record = temp.split(",")
-            print(record)
-            rds_ob.create(record)
+rds = rds_test.rds_handler()
+ACCESS_KEY = '3c0bfa6bfe43cec606d71ea41b842c97'
 
 
-    #PUT method Defination
-    def do_PUT(self):
-        if self.path == '/api/update':
-            temp = self._set_headers()
-            data = temp.split(",")
-            #seprating input into key and value
-            key = data[0]
-            value = data[1]
-            id = data[2]
-            rds_ob = rds_handler()
+def historical_data(city_name, from_time, to_time):
+    #coordinates_lat, coordinates_long,
+    #query = coordinates_lat+","+coordinates_long
+    date_string = ""
+    from_time_ob = datetime.datetime.strptime(from_time, '%Y-%m-%d')
+    to_time_ob = datetime.datetime.strptime(to_time, '%Y-%m-%d')
+    while from_time_ob <= to_time_ob:
+        date_string += from_time_ob.strftime('%Y-%m-%d')+";"
+        from_time_ob = from_time_ob + datetime.timedelta(days=1)
+    date_string = date_string[:-1]
+    # if rds.check_existing(from_time, to_time, city_name) == 1:
+    #     return 0
 
-            rds_ob.update(key,value,id)
-        #check if key is in data
+    historical_date = from_time+";"+to_time
+    params = {
+        'access_key': ACCESS_KEY,
+        'query': city_name,
+        'historical_date': date_string,
+        'hourly': '1',
+        'interval': '1'
+    }
+    api_result = requests.get('https://api.weatherstack.com/historical', params)
+    api_response = api_result.json()
+    # print(api_response)
+    hist_list = []
+    hist_dict = {}
+    lat = api_response["location"]["lat"]
+    lon = api_response["location"]["lon"]
+    city_name = api_response["location"]["name"]
+    zone_id = api_response["location"]["timezone_id"]
+    utc_offset = api_response["location"]["utc_offset"]
 
 
-    #DELETE method defination
-    def do_DELETE(self):
-        if self.path == '/api/delete':
-            temp = self._set_headers()
-            id = temp
-            rds_ob = rds_handler()
-            rds_ob.delete(id)
 
-#Server Initialization
-server = HTTPServer(('127.0.0.1',8080), ServiceHandler)
-server.serve_forever()
+    for data in api_response["historical"]:
+        date = api_response["historical"][data]["date"]
+        for hour in  api_response["historical"][data]["hourly"]:
+            hour_duration = hour["time"]
+            humidity = hour["humidity"]
+            temperature = hour["temperature"]
+            rain = hour["chanceofrain"]
+            wind = hour["wind_speed"]
+
+
+            hist_dict["city_name"]= city_name
+            hist_dict["weather_main"] = hour["weather_descriptions"][0]
+            hist_dict["timestamp"] = date+" "+hour_duration[:-2]
+            hist_dict["lat"] = lat
+            hist_dict["lon"] = lon
+            hist_dict["hour_duration"] = hour_duration
+            hist_dict["humidity"] = humidity
+            hist_dict["temperature"] = temperature
+            hist_dict["rain"] = rain
+            hist_dict["wind"] = wind
+            hist_dict["time_bucket"] = date+"_"+hour_duration+"_"+utc_offset+"_"+zone_id
+
+            hist_list.append((hist_dict.copy()))
+    rds.update_inventory_buy(hist_list,"historical")
+    return hist_list, city_name, "historical",from_time
+
+def forecast_data(city_name):
+
+    params = {
+        'access_key': ACCESS_KEY,
+        'query': city_name,
+        'forecast_days': '2',
+        'hourly':'1',
+        'interval': '1'
+    }
+    api_result = requests.get('https://api.weatherstack.com/forecast', params)
+    api_response = api_result.json()
+    # print(api_response)
+    hist_list = []
+    hist_dict = {}
+    lat = api_response["location"]["lat"]
+    lon = api_response["location"]["lon"]
+    city_name = api_response["location"]["name"]
+    zone_id = api_response["location"]["timezone_id"]
+    utc_offset = api_response["location"]["utc_offset"]
+
+
+    for data in api_response["forecast"]:
+        date = api_response["forecast"][data]["date"]
+        for hour in  api_response["forecast"][data]["hourly"]:
+            hour_duration = hour["time"]
+            humidity = hour["humidity"]
+            temperature = hour["temperature"]
+            rain = hour["chanceofrain"]
+            wind = hour["wind_speed"]
+
+            hist_dict["city_name"]= city_name
+            hist_dict["weather_main"] = hour["weather_descriptions"][0]
+            hist_dict["timestamp"] = date+" "+hour_duration[:-2]
+            hist_dict["lat"] = lat
+            hist_dict["lon"] = lon
+            hist_dict["hour_duration"] = hour_duration
+            hist_dict["humidity"] = humidity
+            hist_dict["temperature"] = temperature
+            hist_dict["rain"] = rain
+            hist_dict["wind"] = wind
+            hist_dict["time_bucket"] = date+"_"+hour_duration+"_"+utc_offset+"_"+zone_id
+
+            hist_list.append((hist_dict.copy()))
+    rds.update_inventory_buy(hist_list,"forecast")
+
+    return hist_list,city_name,"forecast",date
+
+def current_data(city_name):
+
+    # if rds.check_existing(0, 0, city_name,1) == 1:
+    #     return 0
+
+    params = {
+        'access_key': ACCESS_KEY,
+        'query': city_name,
+    }
+    api_result = requests.get('https://api.weatherstack.com/current', params)
+    api_response = api_result.json()
+    print(api_response)
+    hist_list = []
+    hist_dict = {}
+    lat = api_response["location"]["lat"]
+    lon = api_response["location"]["lon"]
+    city_name = api_response["location"]["name"]
+    zone_id = api_response["location"]["timezone_id"]
+    utc_offset = api_response["location"]["utc_offset"]
+    timestamp = api_response["location"]["localtime"][:13]
+
+    humidity = api_response["current"]["humidity"]
+    temperature = api_response["current"]["temperature"]
+    rain = "na"
+    wind = api_response["current"]["wind_speed"]
+
+    hist_dict["city_name"]= city_name
+    hist_dict["weather_main"] = api_response["current"]["weather_descriptions"][0]
+    hist_dict["timestamp"] = timestamp
+    hist_dict["lat"] = lat
+    hist_dict["lon"] = lon
+    # hist_dict["hour_duration"] = hour_duration
+    hist_dict["humidity"] = humidity
+    hist_dict["temperature"] = temperature
+    hist_dict["rain"] = rain
+    hist_dict["wind"] = wind
+    hist_dict["time_bucket"] = timestamp+"_"+utc_offset+"_"+zone_id
+
+    hist_list.append((hist_dict.copy()))
+    rds.update_inventory_buy(hist_list,"current")
+
+    return hist_list,city_name,"current",timestamp
+
+# hist_list = historical_data("New York","2015-01-21","2015-01-25","1")
+
+# hist_list = current_data("Delhi")
+# for item in hist_list:
+#     print(item)
+# def upload_s3(file_name, city_name,types,from_time):
+#     s3.upload_file(
+#         Filename=file_name,
+#         Bucket="weatherjsondump",
+#         Key=types+"_"+from_time+"_"+city_name+".json"
+#     )
+
+
+def loader(event):
+    type = event["type"]
+    if type=="historical":
+        json_list, city_name,types,from_time = historical_data(event["city_name"],event["from_time"],event["to_time"])
+    if type=="forecast":
+        json_list, city_name,types,from_time = forecast_data(event["city_name"])
+    if type=="current":
+        json_list, city_name,types,from_time = current_data(event["city_name"])
+
+    with open('/tmp/result.json', 'w') as fp:
+        json.dump(json_list, fp)
+    # upload_s3('/tmp/result.json',city_name,types,from_time)
+
+def main():
+    event = {}
+    event["type"]= "forecast"
+    event["city_name"] = "New York"
+    event["from_time"] ="2015-01-21"
+    event["to_time"] = "2015-02-21"
+    print(event)
+    loader(event)
+
+
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('SUCCESS')
+    }
+
+if __name__ == "__main__":
+    main()
