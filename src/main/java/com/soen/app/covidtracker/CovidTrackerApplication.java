433bdb7114e9a8071ab5d8fc614e813994b2a75a
Dinesh <dinesh.skini@gmail.com>
2022-11-12 11:42:41 -0500

    Make file structor to have design pattern

---
+++ src/main/java/com/soen/app/covidtracker/CovidTrackerApplication.java
@@ -0,0 +1,9 @@
+package com.soen.app.covidtracker;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/register")
+public class CovidTrackerApplication extends HttpServlet {
+
+}
