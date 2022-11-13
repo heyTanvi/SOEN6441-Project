84772392550d1c6a60e5075827f3a0d66bfc1765
Tannavi Gaurav <37728386+heyTanvi@users.noreply.github.com>
2022-11-13 14:39:52 -0500

    Update CovidTrackerServiceImplTest.java

--- src/test/java/com/soen/app/covidtracker/service/impl/CovidTrackerServiceImplTest.java
+++ src/test/java/com/soen/app/covidtracker/service/impl/CovidTrackerServiceImplTest.java
@@ -6,36 +6,6 @@ import junit.framework.Assert;
 
 public class CovidTrackerServiceImplTest {
 
-	private CovidTrackerServiceImpl covidTrackerServiceImpl = new CovidTrackerServiceImpl();
-
-	@Test
-	public void testInstanceIsCreated() {
-		covidTrackerServiceImpl = new CovidTrackerServiceImpl();
-		Assert.assertNotNull(covidTrackerServiceImpl);
-	}
-
-	@Test
-	public void testGetAllGlobalResult() {
-		String allGlobalResult = covidTrackerServiceImpl.getAllGlobalResult();
-		Assert.assertNotNull(allGlobalResult);
-	}
-
-	@Test
-	public void testGetAllCountryResult() {
-		String allGlobalResult = covidTrackerServiceImpl.getAllCountryResult();
-		Assert.assertNotNull(allGlobalResult);
-	}
-
-	@Test
-	public void testGetCountryResult() {
-		String allGlobalResult = covidTrackerServiceImpl.getCountryResult("AD");
-		Assert.assertNotNull(allGlobalResult);
-	}
-
-	@Test
-	public void testGetAllCountryResultByCode() {
-		String allGlobalResult = covidTrackerServiceImpl.getAllCountryResultByCode("AD,AL,AF,AM,BB,BY,BE,BZ");
-		Assert.assertNotNull(allGlobalResult);
-	}
+	
 
 }
